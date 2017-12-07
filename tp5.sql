7c652a280dca85f0bd4230898be2cfcfc0810d73
crosaalive <diaoling212@qq.com>
2017-12-07 11:45:09 +0800

    sql

---
+++ tp5.sql
@@ -0,0 +1,306 @@
+/*
+Navicat MySQL Data Transfer
+
+Source Server         : localhost
+Source Server Version : 50547
+Source Host           : localhost:3306
+Source Database       : tp5
+
+Target Server Type    : MYSQL
+Target Server Version : 50547
+File Encoding         : 65001
+
+Date: 2017-12-07 11:44:08
+*/
+
+SET FOREIGN_KEY_CHECKS=0;
+
+-- ----------------------------
+-- Table structure for cmf_option
+-- ----------------------------
+DROP TABLE IF EXISTS `cmf_option`;
+CREATE TABLE `cmf_option` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `autoload` tinyint(3) unsigned NOT NULL DEFAULT '1' COMMENT '是否自动加载;1:自动加载;0:不自动加载',
+  `option_name` varchar(64) NOT NULL DEFAULT '' COMMENT '配置名',
+  `option_value` longtext CHARACTER SET utf8 COLLATE utf8_unicode_ci COMMENT '配置值',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `option_name` (`option_name`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 ROW_FORMAT=COMPACT COMMENT='全站配置表';
+
+-- ----------------------------
+-- Records of cmf_option
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for z_auth_access
+-- ----------------------------
+DROP TABLE IF EXISTS `z_auth_access`;
+CREATE TABLE `z_auth_access` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `role_id` int(10) unsigned NOT NULL COMMENT '角色',
+  `rule_name` varchar(100) NOT NULL DEFAULT '' COMMENT '规则唯一英文标识,全小写',
+  `type` varchar(30) NOT NULL DEFAULT '' COMMENT '权限规则分类,请加应用前缀,如admin_',
+  PRIMARY KEY (`id`),
+  KEY `role_id` (`role_id`),
+  KEY `rule_name` (`rule_name`) USING BTREE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='权限授权表';
+
+-- ----------------------------
+-- Records of z_auth_access
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for z_auth_rule
+-- ----------------------------
+DROP TABLE IF EXISTS `z_auth_rule`;
+CREATE TABLE `z_auth_rule` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT COMMENT '规则id,自增主键',
+  `status` tinyint(3) unsigned NOT NULL DEFAULT '1' COMMENT '是否有效(0:无效,1:有效)',
+  `app` varchar(15) NOT NULL COMMENT '规则所属module',
+  `type` varchar(30) NOT NULL DEFAULT '' COMMENT '权限规则分类，请加应用前缀,如admin_',
+  `name` varchar(100) NOT NULL DEFAULT '' COMMENT '规则唯一英文标识,全小写',
+  `param` varchar(100) NOT NULL DEFAULT '' COMMENT '额外url参数',
+  `title` varchar(20) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL DEFAULT '' COMMENT '规则描述',
+  `condition` varchar(200) NOT NULL DEFAULT '' COMMENT '规则附加条件',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `name` (`name`) USING BTREE,
+  KEY `module` (`app`,`status`,`type`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='权限规则表';
+
+-- ----------------------------
+-- Records of z_auth_rule
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for z_category
+-- ----------------------------
+DROP TABLE IF EXISTS `z_category`;
+CREATE TABLE `z_category` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT COMMENT '分类id',
+  `parent_id` bigint(20) unsigned NOT NULL DEFAULT '0' COMMENT '分类父id',
+  `post_count` bigint(20) unsigned NOT NULL DEFAULT '0' COMMENT '分类文章数',
+  `status` tinyint(3) unsigned NOT NULL DEFAULT '1' COMMENT '状态,1:发布,0:不发布',
+  `delete_time` int(10) unsigned NOT NULL DEFAULT '0' COMMENT '删除时间',
+  `list_order` float NOT NULL DEFAULT '10000' COMMENT '排序',
+  `name` varchar(200) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL DEFAULT '' COMMENT '分类名称',
+  `description` varchar(255) NOT NULL COMMENT '分类描述',
+  `seo_title` varchar(100) NOT NULL DEFAULT '',
+  `seo_keywords` varchar(255) NOT NULL DEFAULT '',
+  `seo_description` varchar(255) NOT NULL DEFAULT '',
+  `list_tpl` varchar(50) NOT NULL DEFAULT '' COMMENT '分类列表模板',
+  `one_tpl` varchar(50) NOT NULL DEFAULT '' COMMENT '分类文章页模板',
+  `more` text COMMENT '扩展属性',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='文章分类表';
+
+-- ----------------------------
+-- Records of z_category
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for z_comment
+-- ----------------------------
+DROP TABLE IF EXISTS `z_comment`;
+CREATE TABLE `z_comment` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `parent_id` bigint(20) unsigned NOT NULL DEFAULT '0' COMMENT '被回复的评论id',
+  `user_id` int(10) unsigned NOT NULL DEFAULT '0' COMMENT '发表评论的用户id',
+  `to_user_id` int(10) unsigned NOT NULL DEFAULT '0' COMMENT '被评论的用户id',
+  `object_id` int(10) unsigned NOT NULL DEFAULT '0' COMMENT '评论内容 id',
+  `create_time` int(10) unsigned NOT NULL DEFAULT '0' COMMENT '评论时间',
+  `delete_time` int(10) unsigned NOT NULL DEFAULT '0' COMMENT '删除时间',
+  `status` tinyint(3) unsigned NOT NULL DEFAULT '1' COMMENT '状态,1:已审核,0:未审核',
+  `type` tinyint(3) unsigned NOT NULL DEFAULT '1' COMMENT '评论类型；1实名评论',
+  `table_name` varchar(64) NOT NULL DEFAULT '' COMMENT '评论内容所在表，不带表前缀',
+  `full_name` varchar(50) NOT NULL DEFAULT '' COMMENT '评论者昵称',
+  `email` varchar(255) NOT NULL DEFAULT '' COMMENT '评论者邮箱',
+  `path` varchar(255) NOT NULL DEFAULT '' COMMENT '层级关系',
+  `url` text COMMENT '原文地址',
+  `content` text COMMENT '评论内容',
+  `more` text COMMENT '扩展属性',
+  PRIMARY KEY (`id`),
+  KEY `comment_post_ID` (`object_id`),
+  KEY `comment_approved_date_gmt` (`status`),
+  KEY `comment_parent` (`parent_id`),
+  KEY `table_id_status` (`table_name`,`object_id`,`status`),
+  KEY `createtime` (`create_time`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='评论表';
+
+-- ----------------------------
+-- Records of z_comment
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for z_nav
+-- ----------------------------
+DROP TABLE IF EXISTS `z_nav`;
+CREATE TABLE `z_nav` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `is_enable` tinyint(3) unsigned NOT NULL DEFAULT '1' COMMENT '是否为主导航;1:是;0:不是',
+  `name` varchar(50) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL DEFAULT '' COMMENT '导航位置名称',
+  `remark` varchar(255) NOT NULL DEFAULT '' COMMENT '备注',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='前台导航类别';
+
+-- ----------------------------
+-- Records of z_nav
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for z_nav_menu
+-- ----------------------------
+DROP TABLE IF EXISTS `z_nav_menu`;
+CREATE TABLE `z_nav_menu` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nav_id` int(11) NOT NULL COMMENT '导航 id',
+  `parent_id` int(11) NOT NULL COMMENT '父 id',
+  `status` tinyint(3) unsigned NOT NULL DEFAULT '1' COMMENT '状态;1:显示;0:隐藏',
+  `list_order` float NOT NULL DEFAULT '10000' COMMENT '排序',
+  `name` varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL DEFAULT '' COMMENT '菜单名称',
+  `target` varchar(10) NOT NULL DEFAULT '' COMMENT '打开方式',
+  `href` varchar(200) NOT NULL DEFAULT '' COMMENT '链接',
+  `icon` varchar(20) NOT NULL DEFAULT '' COMMENT '图标',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='前台导航菜单列表';
+
+-- ----------------------------
+-- Records of z_nav_menu
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for z_post
+-- ----------------------------
+DROP TABLE IF EXISTS `z_post`;
+CREATE TABLE `z_post` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `parent_id` int(11) DEFAULT NULL,
+  `post_type` varchar(255) DEFAULT NULL,
+  `post_title` varchar(255) DEFAULT NULL,
+  `post_content` text,
+  `user_id` int(11) NOT NULL,
+  `post_status` varchar(255) DEFAULT NULL,
+  `comment_status` varchar(255) DEFAULT NULL,
+  `post_hits` varchar(255) DEFAULT NULL,
+  `add_time` datetime DEFAULT NULL,
+  `updata_time` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of z_post
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for z_role
+-- ----------------------------
+DROP TABLE IF EXISTS `z_role`;
+CREATE TABLE `z_role` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `parent_id` int(10) unsigned NOT NULL DEFAULT '0' COMMENT '父角色ID',
+  `status` tinyint(3) unsigned NOT NULL DEFAULT '0' COMMENT '状态;0:禁用;1:正常',
+  `create_time` int(10) unsigned NOT NULL DEFAULT '0' COMMENT '创建时间',
+  `update_time` int(10) unsigned NOT NULL DEFAULT '0' COMMENT '更新时间',
+  `list_order` float NOT NULL DEFAULT '0' COMMENT '排序',
+  `name` varchar(20) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL DEFAULT '' COMMENT '角色名称',
+  `remark` varchar(255) NOT NULL DEFAULT '' COMMENT '备注',
+  PRIMARY KEY (`id`),
+  KEY `parentId` (`parent_id`),
+  KEY `status` (`status`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8 COMMENT='角色表';
+
+-- ----------------------------
+-- Records of z_role
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for z_role_user
+-- ----------------------------
+DROP TABLE IF EXISTS `z_role_user`;
+CREATE TABLE `z_role_user` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `role_id` int(10) unsigned NOT NULL DEFAULT '0' COMMENT '角色 id',
+  `user_id` bigint(20) unsigned NOT NULL DEFAULT '0' COMMENT '用户id',
+  PRIMARY KEY (`id`),
+  KEY `group_id` (`role_id`),
+  KEY `user_id` (`user_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='用户角色对应表';
+
+-- ----------------------------
+-- Records of z_role_user
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for z_slide
+-- ----------------------------
+DROP TABLE IF EXISTS `z_slide`;
+CREATE TABLE `z_slide` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `status` tinyint(3) unsigned NOT NULL DEFAULT '1' COMMENT '状态,1:显示,0不显示',
+  `delete_time` int(10) unsigned NOT NULL DEFAULT '0' COMMENT '删除时间',
+  `name` varchar(50) NOT NULL DEFAULT '' COMMENT '幻灯片分类',
+  `remark` varchar(255) NOT NULL DEFAULT '' COMMENT '分类备注',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='幻灯片表';
+
+-- ----------------------------
+-- Records of z_slide
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for z_slide_item
+-- ----------------------------
+DROP TABLE IF EXISTS `z_slide_item`;
+CREATE TABLE `z_slide_item` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `slide_id` int(11) NOT NULL DEFAULT '0' COMMENT '幻灯片id',
+  `status` tinyint(3) unsigned NOT NULL DEFAULT '1' COMMENT '状态,1:显示;0:隐藏',
+  `list_order` float NOT NULL DEFAULT '10000' COMMENT '排序',
+  `title` varchar(50) NOT NULL DEFAULT '' COMMENT '幻灯片名称',
+  `image` varchar(255) NOT NULL DEFAULT '' COMMENT '幻灯片图片',
+  `url` varchar(255) NOT NULL DEFAULT '' COMMENT '幻灯片链接',
+  `target` varchar(10) NOT NULL DEFAULT '' COMMENT '友情链接打开方式',
+  `description` varchar(255) NOT NULL COMMENT '幻灯片描述',
+  `content` text COMMENT '幻灯片内容',
+  `more` text COMMENT '链接打开方式',
+  PRIMARY KEY (`id`),
+  KEY `slide_cid` (`slide_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8 COMMENT='幻灯片子项表';
+
+-- ----------------------------
+-- Records of z_slide_item
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for z_user
+-- ----------------------------
+DROP TABLE IF EXISTS `z_user`;
+CREATE TABLE `z_user` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `user_type` tinyint(3) unsigned NOT NULL DEFAULT '1' COMMENT '用户类型;1:admin;2:会员',
+  `sex` tinyint(2) NOT NULL DEFAULT '0' COMMENT '性别;0:保密,1:男,2:女',
+  `birthday` int(11) NOT NULL DEFAULT '0' COMMENT '生日',
+  `last_login_time` int(11) NOT NULL DEFAULT '0' COMMENT '最后登录时间',
+  `score` int(11) NOT NULL DEFAULT '0' COMMENT '用户积分',
+  `coin` int(10) unsigned NOT NULL DEFAULT '0' COMMENT '金币',
+  `create_time` int(11) NOT NULL DEFAULT '0' COMMENT '注册时间',
+  `user_status` tinyint(3) unsigned NOT NULL DEFAULT '1' COMMENT '用户状态;0:禁用,1:正常,2:未验证',
+  `user_login` varchar(60) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL DEFAULT '' COMMENT '用户名',
+  `user_pass` varchar(64) NOT NULL DEFAULT '' COMMENT '登录密码;cmf_password加密',
+  `user_nickname` varchar(50) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL DEFAULT '' COMMENT '用户昵称',
+  `user_email` varchar(100) NOT NULL DEFAULT '' COMMENT '用户登录邮箱',
+  `user_url` varchar(100) NOT NULL DEFAULT '' COMMENT '用户个人网址',
+  `avatar` varchar(255) NOT NULL DEFAULT '' COMMENT '用户头像',
+  `signature` varchar(255) NOT NULL DEFAULT '' COMMENT '个性签名',
+  `last_login_ip` varchar(15) NOT NULL DEFAULT '' COMMENT '最后登录ip',
+  `user_activation_key` varchar(60) NOT NULL DEFAULT '' COMMENT '激活码',
+  `mobile` varchar(20) NOT NULL DEFAULT '' COMMENT '用户手机号',
+  `more` text COMMENT '扩展属性',
+  PRIMARY KEY (`id`),
+  KEY `user_login` (`user_login`),
+  KEY `user_nickname` (`user_nickname`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='用户表';
+
+-- ----------------------------
+-- Records of z_user
+-- ----------------------------
