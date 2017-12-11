a588c14a08084d588f20fcb6bfcf01fba16834f4
crosaalive <diaoling212@qq.com>
2017-12-11 20:01:51 +0800

    module

--- application/admin/config.php
+++ application/admin/config.php
@@ -1,5 +1,10 @@
 <?php
 //配置文件
 return [
-
+	'layout_on'     =>  true,
+    'layout_name'   =>  'tpl',
+    // 模快名称
+    'model_name' =>'admin',
+    // 默认模板文件名称
+    'default_template' =>'../../public/layui2',        // 这里可以切换模块下的默认模板名称
 ];
--- application/portal/controller/Index.php
+++ application/portal/controller/Index.php
@@ -1,23 +1,27 @@
 <?php
 namespace app\portal\controller;
-
+use think\Controller;
 use think\config;
-class Index
+use think\view;
+use think\Request;
+
+class Index extends Controller
 {
-    public function index($id)
+      public function index(request $request)
     {
-    	// if (isset(IS_GET)) {
-    	// print_r($_SERVER['HTTP_REFERER']) ;
-    		echo $id;
-    	// }
+    	echo input('id')."____<br>";	
+    	$this->assign('id',Request::instance()->param('id'));
     	
     	// dump(Config::get());
-    	echo "<pre>";
-    	print_r(Config::get());
-    	// print_r(\think\Config::get());
-    	// print_r(Config::get('request_cache')) ;
-    	// die;
-    	echo "111";
-        return '<style type="text/css">*{ padding: 0; margin: 0; } div{ padding: 4px 48px;} a{color:#2E5CD5;cursor: pointer;text-decoration: none} a:hover{text-decoration:underline; } body{ background: #fff; font-family: "Century Gothic","Microsoft yahei"; color: #333;font-size:18px;} h1{ font-size: 100px; font-weight: normal; margin-bottom: 12px; } p{ line-height: 1.6em; font-size: 42px }</style><div style="padding: 24px 48px;"> <h1>:)</h1><p> ThinkPHP V5<br/><span style="font-size:30px">十年磨一剑 - 为API开发设计的高性能框架</span></p><span style="font-size:22px;">[ V5.0 版本由 <a href="http://www.qiniu.com" target="qiniu">七牛云</a> 独家赞助发布 ]</span></div><script type="text/javascript" src="http://tajs.qq.com/stats?sId=9347272" charset="UTF-8"></script><script type="text/javascript" src="http://ad.topthink.com/Public/static/client.js"></script><thinkad id="ad_bd568ce7058a1091"></thinkad>';
+    	$cityName = $request->controller();
+        $this->assign('city',$this->city($cityName));
+    	$this->assign('ceshi','momoda');
+    	return $this->fetch('index');
+    }
+
+    protected function city($name)
+    {
+        //	和$name这个城市相关的处理
+         return '当前控制器名字：' . $name;
     }
 }
